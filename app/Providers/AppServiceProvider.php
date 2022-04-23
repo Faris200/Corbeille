f3163906773f9ad5588468b339f380eea6b9a67f
Entertainment-2025 <aaifa200@aol.com>
2022-04-23 15:15:22 +0100

    corbeille 21-04-2020

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+    /*
+    |--------------------------------------------------------------------------
+    | Welcome Mr. Faris
+    |--------------------------------------------------------------------------
+    |
+    */
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ app/Stock.php
@@ -0,0 +1,17 @@
+<?php
+
+    /*
+    |--------------------------------------------------------------------------
+    | Welcome Mr. Faris
+    |--------------------------------------------------------------------------
+    |
+    */
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Stock extends Model
+{
+    
+}
